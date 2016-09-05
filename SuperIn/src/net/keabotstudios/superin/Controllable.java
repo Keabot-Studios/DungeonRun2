e9c87b1131cad1c902b5e4ea3d8c9be93b2970fc
Richard Abbott <richie0415@hotmail.com>
2016-09-05 16:46:46 -0400

    Added SuperIN and SuperLOG

---
+++ SuperIn/src/net/keabotstudios/superin/Controllable.java
@@ -0,0 +1,11 @@
+package net.keabotstudios.superin;
+
+import java.awt.Component;
+
+import net.keabotstudios.superlog.Logger;
+
+public interface Controllable {
+	public Component getComponent();
+	public Logger getLogger();
+	
+}
